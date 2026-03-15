1a6d273ab7171a0346b0b2b4ec151869313770bf
suhaibfida <suhaibfida33@outlook.com>
2026-03-15 16:31:52 +0530

    img

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 A digital brain dump where your ideas, chores, and random midnight thoughts finally find a home instead of floating away.
+![alt text](image.png)
--- packages/ui/src/Navbar.tsx
+++ packages/ui/src/Navbar.tsx
@@ -1,9 +1,9 @@
 export function Navbar() {
   return (
     <>
-      <div className="flex justify-between bg-gray-300  py-3 border border-purple-900 border-5 rounded-full">
+      <div className="flex justify-between bg-gray-300 border border-purple-900 border-5 rounded-full">
         <div className="flex">
-          <div className="flex font-bold text-purple-800 text-3xl pl-5">
+          <div className="flex font-bold text-purple-800 text-2xl pl-5">
             <div className="text-blue-600 text-5xl">t</div>
             <span className="pt-3">hinker..</span>
           </div>
