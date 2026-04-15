8e098ec694d0140b2a57d0943da0533c7302d855
suhaibfida <suhaibfida33@outlook.com>
2026-04-15 14:23:48 +0530

    prisma init

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import router from "./router/router";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1", router);
 app.listen(3000);
