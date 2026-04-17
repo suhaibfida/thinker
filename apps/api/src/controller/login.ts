88c9fd4078c6dce61123867763ecc1fa7157fa7a
suhaibfida <suhaibfida33@outlook.com>
2026-04-17 22:54:23 +0530

    auth/login

--- apps/api/src/controller/login.ts
+++ apps/api/src/controller/login.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import prisma from "@repo/db/prisma";
+import "dotenv/config";
 import { loginSchema } from "@repo/zod/loginSchema";
+import bcrypt from "bcrypt";
 const login = async (req: Request, res: Response) => {
+  const jwtSecret = process.env.JWT_SECRET;
   const safeParse = loginSchema.safeParse(req.body);
   if (!safeParse.success) {
     return res.status(400).json({
@@ -15,17 +18,36 @@ const login = async (req: Request, res: Response) => {
   } else {
     username = safeParse.data.usernameOrEmail;
   }
-  const user = await prisma.user.findFirst({
+  console.log(email);
+  console.log(username);
+
+  const find = await prisma.user.findFirst({
     where: {
       OR: [{ username: username }, { email: email }],
-      password: safeParse.data.password,
     },
   });
-  if (user) {
+  if (!find) {
+    return res.status(400).json({
+      message: "User doesn't exist",
+    });
   }
+  const compare = bcrypt.compare(safeParse.data.password, find.password);
+  if (!compare) {
+    return res.status(400).json({
+      message: "Password is incorrect",
+    });
+  }
+
+  res.cookie("token", find.id, {
+    httpOnly: true,
+    secure: process.env.LIVE === "production" ? true : false,
+    sameSite: "lax",
+    maxAge: 28 * 24 * 60 * 60 * 1000,
+  });
+
   return res.status(400).json({
     message: "User logged in successfully",
-    username: user.username,
+    username: find.username,
   });
 };
 export default login;
--- apps/api/src/router/router.ts
+++ apps/api/src/router/router.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { signup } from "../controller/signup.js";
-
+import login from "../controller/login.js";
 const router: Router = Router();
 router.post("/auth/register", signup);
-// router.post("auth/login");
+router.post("/auth/login", login);
 
 export default router;
